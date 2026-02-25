eef3e964948f5df0b44020a0a6a43f46a7c16002
Nithin0306 <nithin.p.m2006@gmail.com>
2026-02-25 11:19:48 +0530

    add: basic Processes

---
+++ 01_Processes/fork_exec.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        execlp("ls", "ls", NULL);
+    }
+    else {
+        wait(NULL);
+        printf("Child finished\n");
+    }
+
+    return 0;
+}
